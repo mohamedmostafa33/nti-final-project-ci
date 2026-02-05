983eb1d916545eec1cd539f804e0a168d5f80d52
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-02-05 14:05:18 +0000

    fix: disable S3 ACLs for bucket policy based access

--- src/backend/reddit_api/storage_backends.py
+++ src/backend/reddit_api/storage_backends.py
@@ -7,11 +7,13 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     """Storage backend for static files"""
     location = 'static'
-    default_acl = 'public-read'
+    default_acl = None
+    querystring_auth = False
 
 
 class MediaStorage(S3Boto3Storage):
     """Storage backend for media files (user uploads)"""
     location = 'media'
-    default_acl = 'public-read'
+    default_acl = None
+    querystring_auth = False
     file_overwrite = False
